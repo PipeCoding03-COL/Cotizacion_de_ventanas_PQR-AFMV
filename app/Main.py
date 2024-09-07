530aab75a282fc7f2180b29d27bb028c26fa2031
PipeCoding03-COL <AFMV3007@gmail.com>
2024-09-07 07:49:22 -0500

    Creacion del menu

--- app/Main.py
+++ app/Main.py
@@ -1,1 +1,11 @@
-print("¿Qué tipo de ventanas desea?")
+print("¡Hola! Bienvenido a este nuevo software de PQR S.A..")
+print("Siga atentamente las instrucciones para generar su cotización de ventanas con nosotros.\n")
+
+#Solicitar el estilo de ventana al usuario y validarlo.
+estilosVentanas = ["O", "XO", "OXO", "OXXO"]
+while True:
+    tipoVentana = input("¿Qué tipo de ventanas desea?: (O, XO, OXO, OXXO)")
+    if tipoVentana in estilosVentanas:
+        print("Correcto. Continuemos.")
+    else:
+        print("El estilo seleccionado no es válido. Por favor, ingrese una de las opciones: O, XO, OXO, OXXO")
