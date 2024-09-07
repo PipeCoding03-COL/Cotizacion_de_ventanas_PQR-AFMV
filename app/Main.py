78fa8a6cce6b84cc1d4d4dc6ab0d127f399a499c
PipeCoding03-COL <AFMV3007@gmail.com>
2024-09-07 08:41:58 -0500

    Menú #2

--- app/Main.py
+++ app/Main.py
@@ -1,16 +1,23 @@
 print("¡Hola! Bienvenido a este nuevo software de PQR S.A..")
 print("Siga atentamente las instrucciones para generar su cotización de ventanas con nosotros.\n")
 
-#Solicitar el estilo de ventana al usuario y validarlo.
 estilosVentanas = ["O", "XO", "OXO", "OXXO"]
+acabadosAluminio = ["Pulido", "Lacado brillante", "Lacado mate", "Anodizado"]
+
+#Solicitar al usuario el estilo de ventana y validarlo.
 while True:
-    tipoVentana = input("¿Qué tipo de ventanas desea?: (O, XO, OXO, OXXO)")
+    tipoVentana = input("¿Qué tipo de ventanas desea? (O, XO, OXO, OXXO):")
     if tipoVentana in estilosVentanas:
-        print("Listado de acabados disponibles para el aluminio:")
-        print("1. Pulido.")
-        print("2. Lacado brillante.")
-        print("3. Lacado mate.")
-        print("4. Anodizado.")
-        tipoAcabado = input("\n¿Qué tipo de acabado desea para el aluminio?:")
+        break 
     else:
         print("El estilo seleccionado no es válido. Por favor, ingrese una de las opciones: O, XO, OXO, OXXO.")
+
+#Solicitar al usuario el acabado del aluminio y validarlo.
+print("Listado de acabados disponibles para el aluminio:")
+print("1. Pulido.")
+print("2. Lacado brillante.")
+print("3. Lacado mate.")
+print("4. Anodizado.")
+tipoAcabado = int(input("\n¿Qué tipo de acabado desea para el aluminio?:"))
+
+print(f"Ha seleccionado: {acabadosAluminio[tipoAcabado]}")
