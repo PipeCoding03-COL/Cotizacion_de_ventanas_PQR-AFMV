ac513e0b894187e0ed039847fad91c194cc154a5
PipeCoding03-COL <AFMV3007@gmail.com>
2024-09-07 08:28:28 -0500

    Continuación del menú #1

--- app/Main.py
+++ app/Main.py
@@ -6,6 +6,11 @@ estilosVentanas = ["O", "XO", "OXO", "OXXO"]
 while True:
     tipoVentana = input("¿Qué tipo de ventanas desea?: (O, XO, OXO, OXXO)")
     if tipoVentana in estilosVentanas:
-        print("Correcto. Continuemos.")
+        print("Listado de acabados disponibles para el aluminio:")
+        print("1. Pulido.")
+        print("2. Lacado brillante.")
+        print("3. Lacado mate.")
+        print("4. Anodizado.")
+        tipoAcabado = input("\n¿Qué tipo de acabado desea para el aluminio?:")
     else:
-        print("El estilo seleccionado no es válido. Por favor, ingrese una de las opciones: O, XO, OXO, OXXO")
+        print("El estilo seleccionado no es válido. Por favor, ingrese una de las opciones: O, XO, OXO, OXXO.")
